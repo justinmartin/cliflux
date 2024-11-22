e70b07c7938c673392f7b2644cedacb201e55196
Justin Martin <jus10mar10@gmail.com>
2024-11-22 10:35:28 -0600

    initial nixpkg flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Rust package flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "cliflux";
+          version = "v1.4.4";
+          src = ./.;
+          cargoHash = "sha256-TtK1hN1RNusVWxWVTity+N1cpfauhfeQpkqAOI1fIco=";
+        };
+      }
+    );
+}
